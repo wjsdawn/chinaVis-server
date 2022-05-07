b7b5327efaec2e77000389d671665caa41b1ed0f
wjsdawn <1666938053@qq.com>
2022-05-07 18:20:00 +0800

    数据库地址修改为服务器地址

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const neo4j =require('neo4j-driver')
-const db = "bolt://127.0.0.1:7687"
+// const db = "bolt://127.0.0.1:7687"
+const db = 'bolt://47.111.179.56:7687'
 const user = 'neo4j'
 const password ='123456'
 const driver =neo4j.driver(db,neo4j.auth.basic(user,password),{maxTransactionRetryTime:3000})
